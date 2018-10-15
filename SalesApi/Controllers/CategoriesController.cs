2100bca9e64e201308edba93cd2ceb62815f5663
emilio barrera <barrera_emilio@yahoo.es>
2018-10-16 00:11:28 +0100

    add categories controller for  backend and api

---
+++ SalesApi/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Sales.Common.Models;
+using Sales.Domain.Models;
+using System.Linq;
+using System.Web.Http;
+
+
+[Authorize]
+public class CategoriesController : ApiController
+{
+    private DataContext db = new DataContext();
+
+    public IQueryable<Category> GetCategories()
+    {
+        return db.Categories.OrderBy(c => c.Description);
+    }
+}
